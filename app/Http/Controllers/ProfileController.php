c1dfb81f8e3a6c586c7f2f7139fc6a7dbc975a57
Alejandro <alexdev.gc@gmail.com>
2024-09-25 13:36:19 -0500

    solving password update issue

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -19,51 +19,51 @@ class ProfileController extends Controller
     public function store(Request $request)
     {
         // modify the request
-        $request->request->add(['username'=>Str::slug($request->username)]);
+        $request->request->add(['username' => Str::slug($request->username)]);
 
         $this->validate($request, [
-            'username'=>['required','unique:users,username,'.Auth::user()->id,'min:3','max:30', 'not_in:twitter,facebook,instagram,editar-perfil'],
+            'username' => ['required', 'unique:users,username,' . Auth::user()->id, 'min:3', 'max:30', 'not_in:twitter,facebook,instagram,editar-perfil'],
+            'name' => ['required', 'string', 'max:255']
         ]);
 
 
-        if($request->image)
+        if ($request->image) 
         {
-            
             $image = $request->file('image');
 
             $nameImage = Str::uuid() . "." . $image->extension();
-    
+
             $imageServer = Image::read($image);
             $imageServer->cover(1000, 1000);
-    
+
             $imagePath = public_path('profiles') . "/" . $nameImage;
             $imageServer->save($imagePath);
+        }
 
-            if($request->password || $request->current_password)
-            {
-                $this->validate($request, [
-                    'password'=>'required|confirmed|min:10'
-                ]);
-    
-                if (Hash::check($request->oldpassword, Auth::user()->password)) 
-                {
-                    $user = User::find(Auth::user()->id);
-                    $user->password = Hash::make($request->password) ?? Auth::user()->password;
-                } else {
-                    return back()->with('message', 'The current password is wrong');
-                }
+        if ($request->password || $request->current_password) {
+            $this->validate($request, [
+                'current_password' => 'required',
+                'password' => 'required|confirmed|min:10',
+            ]);
+
+            if (Hash::check($request->current_password, Auth::user()->password)) {
+                // Update the password
+                Auth::user()->password = Hash::make($request->password);
+            } else {
+                // Return an error if the current password doesn't match
+                return back()->with('message', 'The current password is incorrect');
             }
-            
-            // Save Changes
-            $user = User::find(Auth::user()->id);
-            $user->name = $request->name ?? Auth::user()->name;
-            $user->username = $request->username ?? Auth::user()->username;
-            $user->email = $request->email ?? Auth::user()->email;
-            $user->image = $nameImage ?? Auth::user()->image ?? null;
-            $user->save();
+        }
 
-            // Redirect 
-            return redirect()->route('posts.index', $user->username);
-        } 
+        // Save Changes
+        $user = User::find(Auth::user()->id);
+        $user->name = $request->name ?? Auth::user()->name;
+        $user->username = $request->username ?? Auth::user()->username;
+        $user->email = $request->email ?? Auth::user()->email;
+        $user->image = $nameImage ?? Auth::user()->image ?? null;
+        $user->save();
+
+        // Redirect 
+        return redirect()->route('posts.index', $user->username);
     }
 }
